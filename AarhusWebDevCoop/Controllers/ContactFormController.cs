34df46182e1b45f3642cd6c3f8aa29d443b1104a
felix1991 <felix91@live.dk>
2016-03-15 12:01:55 +0100

    Uploading project

---
+++ AarhusWebDevCoop/Controllers/ContactFormController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormController : SurfaceController
+    {
+        // GET: ContactForm
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm");
+        }
+    }
+}
